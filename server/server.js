f97468d34e8dfa5027e2bef409b1b7d96549a332
abdulrafay-1 <abdulrock413@gmail.com>
2025-07-24 16:13:54 +0500

    added cors headers

--- server/server.js
+++ server/server.js
@@ -17,6 +17,16 @@ const webhookRoutes = require("./routes/webhookRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 
 app.use("/api/webhook", webhookRoutes);
+// Add CORS headers for image requests
+app.use("/uploads", (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://checkmymenu.com");
+  res.header("Access-Control-Allow-Methods", "GET");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use("/uploads", express.static("uploads"));
 
 app.use("/api/auth", authRoutes);
